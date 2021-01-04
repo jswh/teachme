5bd8fbd6fb5c19ad8de22c88079a6ce0298965e8
jswh <jswh@jswh.me>
2021-01-04 16:18:35 +0800

    use ts

---
+++ src/Redis.ts
@@ -0,0 +1,17 @@
+import { promisify } from 'util'
+import { createClient } from 'redis'
+const client = createClient({
+  url: process.env.REDIS_URL
+});
+
+client.on("error", function (error) {
+  console.error(error);
+});
+
+const get = promisify(client.get).bind(client)
+const setex = promisify(client.setex).bind(client)
+
+export default {
+  get,
+  setex
+}
---
+++ src/Server.ts
@@ -0,0 +1,75 @@
+import * as express from "express";
+import redis from './Redis'
+import { createServer, Server as HttpServer } from 'http'
+import { Socket } from 'socket.io';
+
+export default class Server {
+  express!: express.Application
+  httpServer!: HttpServer
+  socketio!: Socket
+
+
+  socketRepo: { [sid: string]: Socket } = {}
+  constructor() {
+    let options = {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+      }
+    };
+    this.express = express()
+    this.httpServer = createServer(this.express)
+    this.socketio = require('socket.io')(this.httpServer, options)
+
+    this.socketio.on('connection', this.onConnection.bind(this))
+  }
+
+  onConnection(socket: Socket) {
+    console.log('connected:' + socket.id)
+    socket.on("is_auth", this.onAuth.bind({ socket, server: this }))
+    socket.on("message", this.onMessage.bind(this))
+    socket.on("disconnect", () => {
+      console.log('disconnected:' + socket.id)
+      if (this.socketRepo[socket.id]) {
+        delete this.socketRepo[socket.id]
+      }
+    })
+
+  }
+  async onAuth(uid) {
+    let app = this as unknown as { socket: Socket, server: Server }
+    await redis.setex(uid, 3600, 'verifie')
+    let res = await redis.get(uid)
+    if (res) {
+      console.log('authed:' + uid + ':' + app.socket.id)
+      await redis.setex(uid, 600, app.socket.id)
+      app.server.socketRepo[app.socket.id] = app.socket
+      setTimeout(() => {
+        app.socket.emit("is_auth_ok", 'ok')
+      }, 1000);
+    } else {
+      app.socket.disconnect()
+    }
+
+  }
+  async onMessage(from, to, text) {
+    console.log(from + '->' + to + ':' + text)
+    let toSocketId = await redis.get(to)
+    this.socketRepo[toSocketId].emit("message", from, text)
+  }
+
+
+
+  start() {
+    const port = process.env.PORT || 3000
+
+    this.express.get('/ping', () => {
+      return 'pong!'
+    })
+
+    this.httpServer.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`)
+    })
+
+  }
+}
