43f5d7540f87cf78e3f03d6eedd0ffab8cc76008
jswh <jswh@jswh.me>
2021-01-06 22:33:39 +0800

    use new redirect uri

--- app/Http/Controllers/Api/LineController.php
+++ app/Http/Controllers/Api/LineController.php
@@ -18,7 +18,7 @@ class LineController extends ApiController
             'form_params' => [
                 'grant_type'    => 'authorization_code',
                 'code'          => $code,
-                'redirect_uri'  => 'http://localhost:8080/#/withline',
+                'redirect_uri'  => 'http://localhost:8080/withline',
                 'client_id'     => '1655551351',
                 'client_secret' => getenv('LINE_SECRET')
             ]
